641157312df933c0c9eefb8e290cf5b9e79416a8
Hélder Almeida <hcalmeida@uesc.br>
2023-03-16 20:15:37 -0300

    [debug] mais teste de deploy

--- deploy_github.php
+++ deploy_github.php
@@ -1,1 +1,15 @@
-<?php `git pull`;
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8"/>
+<title>Usando GIT para atualizar arquivos</title>
+</head>
+<body>
+	<pre>
+	<?php
+		$exec = shell_exec("git pull");
+		echo $exec;
+	?>
+	</pre>
+</body>
+</html>
